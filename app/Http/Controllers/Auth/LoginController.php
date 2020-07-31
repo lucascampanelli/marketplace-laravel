534df123aea65fe7488813bb6be1080ee5e29658
Lucas Campanelli <lucas.campanelli@outlook.com>
2020-07-31 02:21:58 -0300

    Changing redirect after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/admin/products';
 
     /**
      * Create a new controller instance.
