357e44060e4e20e6b11e4411a84460ef086565eb
Lucas Campanelli <lucas.campanelli@outlook.com>
2020-08-16 23:26:25 -0300

    Fixing photo remove

--- app/Http/Controllers/Admin/ProductPhotoController.php
+++ app/Http/Controllers/Admin/ProductPhotoController.php
@@ -11,7 +11,7 @@ class ProductPhotoController extends Controller
 {
     public function removePhoto(Request $request)
     {
-        $photoName = $request->get['photoName'];
+        $photoName = $request->get('photoName');
 
         //Remover a foto dos arquivos 
         if(Storage::disk('public')->exists($photoName)){
@@ -23,7 +23,6 @@ class ProductPhotoController extends Controller
         $productId = $removePhoto->first()->product_id;
 
         $removePhoto->delete();
-
         flash('Imagem removida com sucesso!')->success();
         return redirect()->route('admin.products.edit', ['product' => $productId]); //Retorna para a página de edição do produto escolhido
 
